775c87771be75b308f2d5fd513693f222cddd24e
Александр Лисунов <lisunov_alexandr@mail.ru>
2023-06-12 11:54:45 +0300

    Добавляет функцию

--- js/functions.js
+++ js/functions.js
@@ -5,11 +5,16 @@ console.log(checkStrLength('проверяемая строка', 20));
 
 // Функция для проверки, является ли строка палиндромом
 const checkPalindrome = (string) => {
-  string = String(string).toUpperCase().replaceAll(' ','');
+  string = String(string).toUpperCase().replaceAll(' ', '');
   return string === string.split('').reverse().join('');
 };
 
 console.log(checkPalindrome('Лёша на полке клопа нашёл '));
 
 // Дополнительное задание
+const extractingNumbers = function (stringName) {
+  stringName = parseInt(String(stringName).replace(/[^0-9]+/g, ''), 10);
+  return stringName;
+};
 
+console.log(extractingNumbers('ECMAScript 2022'));

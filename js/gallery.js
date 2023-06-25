27544e94737b93a0fa8cb1a9c63b703966779765
Александр Лисунов <lisunov_alexandr@mail.ru>
2023-06-25 15:03:51 +0300

    Исправляет ошибки

--- js/gallery.js
+++ js/gallery.js
@@ -5,6 +5,23 @@ const galleryList = document.querySelector('.pictures');
 const galleryTemplate = document.querySelector('#picture').content.querySelector('.picture');
 
 // Отрисовка фотографий в миниатюре
+// const renderPosts = (dataPictures) => {
+//   const listFragment = document.createDocumentFragment();
+
+//   dataPictures.forEach(({ url, description, likes, comments, id }) => {
+//     const templateElement = galleryTemplate.cloneNode(true);
+//     templateElement.querySelector('.picture__img').src = url;
+//     templateElement.querySelector('.picture__img').alt = description;
+//     templateElement.querySelector('.picture__likes').textContent = likes;
+//     templateElement.querySelector('.picture__comments').textContent = comments.length;
+//     templateElement.dataset.thumbnailId = id;
+
+//     listFragment.append(templateElement);
+//   });
+
+//   galleryList.append(listFragment);
+// };
+
 const galleryData = createPosts();
 const createPost = (dataPictures) => {
   const postElement = galleryTemplate.cloneNode(true);
